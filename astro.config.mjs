d084303a8ca39ff3f4c957211f65ba05fc89a6c5
rambrosteam <protected>
2024-10-14 21:45:21 +0530

    update starlight plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,45 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightImageZoom from 'starlight-image-zoom'
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
+import starlightLinksValidator from 'starlight-links-validator'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rambros3d.github.io',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
+			plugins: [
+				starlightLinksValidator(),
+				starlightUtils({
+					navLinks: {
+						leading: { useSidebarLabelled: "leadingNavLinks" }
+					},
+				}),
+				starlightImageZoom({ showCaptions: true })
+			],
+			title: 'RamBros',
+			logo: {
+				light: '/src/assets/rambros-dragon-logo/rambros-dragon.svg',
+				dark: '/src/assets/rambros-dragon-logo/rambros-dragon.svg',
+				//replacesTitle: true,
 			},
+			social: {
+				github: 'https://github.com/rambros3d',
+				youtube: 'https://www.youtube.com/@RamBrosWorkshop',
+				discord: 'https://discord.gg/pS29QSZF',
+			},
+			customCss: process.env.NO_GRADIENTS ? [] : ['./src/assets/styles/landing.css'],
 			sidebar: [
+				{ label: "DragonRailway", autogenerate: { directory: 'dragon-railway' }, },
+				{ label: "TWYN", autogenerate: { directory: 'twyn' }, },
 				{
-					label: 'Guides',
+					label: "leadingNavLinks",
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: "TWYN", link: "/twyn" },
+						{ label: "DragonRailway", link: "/dragon-railway" },
 					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
